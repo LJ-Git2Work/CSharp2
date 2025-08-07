06bdac50576a7a85991a541aa4d2ca61dce48982
unknown <30105820@tafe.wa.edu.au>
2025-08-07 12:16:28 +0800

    Made smaller, removed unnecessary methods, made comments

--- LiteratePrimes.cs
+++ LiteratePrimes.cs
@@ -11,67 +11,48 @@ namespace literatePrimes
         {
             primes = new int[n];
             multiplesOfPrimeFactors = new List<int>();
-            set2AsFirstPrime();
-            checkOddNumbersForSubsequentPrimes();
-            return primes;
-        }
-
-        private static void set2AsFirstPrime()
-        {
+            
             primes[0] = 2;
             multiplesOfPrimeFactors.Add(2);
-        }
 
-        private static void checkOddNumbersForSubsequentPrimes()
-        {
             int primeIndex = 1;
             for (int candidate = 3; primeIndex < primes.Length; candidate += 2)
             {
                 if (isPrime(candidate))
                     primes[primeIndex++] = candidate;
             }
+
+            return primes;
         }
 
         private static bool isPrime(int candidate)
         {
-            if (isLeastRelevantMultipleOfLargerPrimeFactor(candidate))
+            // Shorten variable names, these are too long
+            // Add comments to explain how the list of multiples does its confusing
+            int nextLargerPrimeFactor = primes[multiplesOfPrimeFactors.Count];
+            int leastRelevantMultiple = nextLargerPrimeFactor * nextLargerPrimeFactor;
+
+            if (candidate == leastRelevantMultiple)
             {
                 multiplesOfPrimeFactors.Add(candidate);
                 return false;
             }
-            return isNotMultipleOfAnyPreviousPrimeFactor(candidate);
-        }
-
-        private static bool isLeastRelevantMultipleOfLargerPrimeFactor(int candidate)
-        {
-            int nextLargerPrimeFactor = primes[multiplesOfPrimeFactors.Count];
-            int leastRelevantMultiple = nextLargerPrimeFactor * nextLargerPrimeFactor;
-            return candidate == leastRelevantMultiple;
-        }
-
-        private static bool isNotMultipleOfAnyPreviousPrimeFactor(int candidate)
-        {
-            for (int n = 1; n < multiplesOfPrimeFactors.Count; n++)
+            else 
             {
-                if (isMultipleOfNthPrimeFactor(candidate, n))
-                    return false;
+                for (int n = 1; n < multiplesOfPrimeFactors.Count; n++)
+                {
+                    int multiple = multiplesOfPrimeFactors[n];
+                    while (multiple < candidate)
+                        multiple += 2 * primes[n];
+                    multiplesOfPrimeFactors[n] = multiple;
+
+                    if (candidate == multiple)
+                        return false;
+                }
+                return true;
             }
-            return true;
         }
 
-        private static bool isMultipleOfNthPrimeFactor(int candidate, int n)
-        {
-            return candidate ==
-                   smallestOddNthMultipleNotLessThanCandidate(candidate, n);
-        }
-
-        private static int smallestOddNthMultipleNotLessThanCandidate(int candidate, int n)
-        {
-            int multiple = multiplesOfPrimeFactors[n];
-            while (multiple < candidate)
-                multiple += 2 * primes[n];
-            multiplesOfPrimeFactors[n] = multiple;
-            return multiple;
-        }
+        // Removed unnecessary methods, keep the logic tight
     }
 }
